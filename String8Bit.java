2abd03d180550c41c703314dd58fdd35ac831baf
Daniël Mizrahi <dmizrahi@live.nl>
2016-12-26 22:12:20 +0100

    Initial work
    The part I already developed before it started it's life on GitHub.

---
+++ String8Bit.java
@@ -0,0 +1,55 @@
+public class String8Bit {
+	private byte[] str;
+
+	public String8Bit() {
+		str = new byte[0];
+	}
+	
+	public String8Bit(String str) {
+		this.str = str.getBytes();
+	}
+
+	public String8Bit(byte[] str) {
+		System.arraycopy(str, 0, this.str, 0, str.length);
+	}
+	
+	public String8Bit(char[] str) {
+		this.str = new byte[str.length];
+		System.arraycopy(str, 0, this.str, 0, str.length);
+	}
+
+	public int length() {
+		return str.length;
+	}
+
+	public char charAt(int index) throws IndexOutOfBoundsException {
+		if (index < 0 || index > str.length) {
+			throw new IndexOutOfBoundsException("Admirable but mistaken.");
+		}
+		if (str[index] < 0) {
+			return (char) (-str[index] + 128);
+		} else {
+			return (char) str[index];
+		}
+	}
+	
+	public String8Bit concat(String str) {
+		byte[] concatstr = new byte[this.str.length + str.length()];
+		System.arraycopy(this.str, 0, concatstr, 0, this.str.length);
+		for (int i = this.str.length + 1; i < this.str.length + str.length(); i++) {
+			concatstr[i] = (byte) str.charAt(i - this.str.length);
+		}
+		return new String8Bit(concatstr);
+	}
+
+	public String8Bit concat(String8Bit str) {
+		byte[] concatstr = new byte[this.str.length + str.length()];
+		System.arraycopy(this.str, 0, concatstr, 0, this.str.length);
+		System.arraycopy(str, 0, concatstr, this.str.length + 1, str.length());
+		return new String8Bit(concatstr);
+	}
+
+	public String toString() {
+		return new String(str);
+	}
+}
