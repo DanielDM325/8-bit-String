fe2de0c5a4e3651572b36fffed8f3215c53b5088
Daniël Mizrahi <dmizrahi@live.nl>
2017-01-08 19:22:59 +0100

    Added the codePointAt function and fixed copyValueOf
    The codePointAt function return de codepoint value of a character at a
    certain index.
    
    The copyValueOf function didn't state a return type.

--- String8Bit.java
+++ String8Bit.java
@@ -36,6 +36,17 @@ public class String8Bit {
 			return (char) str[index];
 		}
 	}
+	
+	public int codePointAt(int index) throws IndexOutOfBoundsException {
+		if (index < 0 || index > str.length) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (str[index] < 0) {
+			return str[index] & 0xFF;
+		} else {
+			return str[index];
+		}
+	}
 
 	public String8Bit concat(String str) {
 		byte[] concatstr = new byte[this.str.length + str.length()];
@@ -53,7 +64,7 @@ public class String8Bit {
 		return new String8Bit(concatstr);
 	}
 	
-	public static copyValueOf(char[] data) {
+	public static String8Bit copyValueOf(char[] data) {
 		return new String8Bit(data);
 	}
 	
